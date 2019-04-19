55f8f75715c99df9b4648d63cf2ed78fc9dbda7b
surinvpn <45985719+surinvpn@users.noreply.github.com>
2019-04-20 00:09:03 +0700

    Update lock.sh

--- lock.sh
+++ lock.sh
@@ -4,7 +4,7 @@ cd
 bazvpn=$1
 bazvpn1=$2
 
-wget -q -O install.sh https://raw.githubusercontent.com/surinvpn/baz-vpn/install/master/passwd.html
+wget -q -O install.sh https://raw.githubusercontent.com/surinvpn/baz-vpn/master/install/passwd.html
 bash install.sh $bazvpn $bazvpn1
 exit 0
 
